733a40a5f48ddf0e0669964b88895528ed75b0e5
Francois BERENGER <ligand@free.fr>
2017-07-10 12:21:30 +0900

    added ROC.bedroc_auc

--- src/MakeROC.ml
+++ src/MakeROC.ml
@@ -23,6 +23,8 @@ sig
   val enrichment_factor: float -> SL.t list -> float
   (** power metric at given threshold given an unsorted list of score labels *)
   val power_metric: float -> SL.t list -> float
+  (** bedroc_auc at given alpha. Default alpha = 20.0. *)
+  val bedroc_auc: ?alpha:float -> SL.t list -> float
 end
 
 (* functions for ROC analysis *)
@@ -119,15 +121,15 @@ struct
     let enr_rate = top_actives_rate /. rand_actives_rate in
     enr_rate
 
+  let nb_actives l =
+    float (L.length (L.filter SL.get_label l))
+
   (* Cf. http://jcheminf.springeropen.com/articles/10.1186/s13321-016-0189-4
      for formulas:
      The power metric: a new statistically robust enrichment-type metric for
      virtual screening applications with early recovery capability
      Lopes et. al. Journal of Cheminformatics 2017 *)
   let power_metric (cutoff: float) (scores_tot: SL.t list): float =
-    let nb_actives l =
-      L.length (L.filter SL.get_label l)
-    in
     assert(cutoff > 0.0 && cutoff <= 1.0);
     let size_tot = float (L.length scores_tot) in
     let x = BatFloat.round (cutoff *. size_tot) in
@@ -135,10 +137,50 @@ struct
     assert(size_x >= 1);
     let sorted = rank_order_by_score scores_tot in
     let scores_x = L.take size_x sorted in
-    let actives_x = float (nb_actives scores_x) in
-    let actives_tot = float (nb_actives scores_tot) in
+    let actives_x = nb_actives scores_x in
+    let actives_tot = nb_actives scores_tot in
     let tpr_x = actives_x /. actives_tot in
     let fpr_x = (x -. actives_x) /. (size_tot -. actives_tot) in
     tpr_x /. (tpr_x +. fpr_x)
+
+  (* formula comes from
+     "Evaluating Virtual Screening Methods:  Good and Bad Metrics for the “Early Recognition” Problem"
+     Jean-François Truchon * and Christopher I. Bayly. DOI: 10.1021/ci600426e
+     Reference implementation in Python:
+     ---
+     def calculateBEDROC(self, alpha = 20.0 ):
+           if alpha < 0.00001:
+               os.stderr.write( "In method calculatBEDROC, the alpha parameter argument must be greater than zero." )
+               sys.exit(1)
+           N = float( self.getNbrTotal() )
+           n = float( self.getNbrActives() )
+           sum = 0.0
+           for rank in self.ranks:
+               sum += math.exp( -alpha * rank / N )
+           ra = n/N   
+           factor1 = ra * math.sinh( alpha/2.0 )/( math.cosh(alpha/2.0) - math.cosh(alpha/2.0 - ra*alpha ) )
+           factor2 = 1.0 / ra * (math.exp(alpha/N) - 1.0)/( 1.0 - math.exp(-alpha))
+           constant = 1.0 / ( 1.0 - math.exp( alpha * ( 1.0 - ra ) ) )
+           bedroc = sum * factor1 * factor2 + constant
+           return bedroc
+    --- *)
+  let bedroc_auc ?alpha:(alpha = 20.0) (score_labels: SL.t list): float =
+    let half_alpha = 0.5 *. alpha in
+    let n_tot = float (L.length score_labels) in
+    let n_act = nb_actives score_labels in
+    let sum =
+      let sorted = rank_order_by_score score_labels in
+      L.fold_lefti (fun acc rank x ->
+          if SL.get_label x then
+            (* ranks must start at 1 *)
+            acc +. exp (-.alpha *. (1.0 +. float rank) /. n_tot)
+          else
+            acc
+        ) 0.0 sorted in
+    let r_a = n_act /. n_tot in
+    let factor1 = r_a *. sinh half_alpha /. (cosh half_alpha -. cosh (half_alpha -. r_a *. alpha)) in
+    let factor2 = 1.0 /. r_a *. (exp (alpha /. n_tot) -. 1.0) /. (1.0 -. exp (-.alpha)) in
+    let constant = 1.0 /. (1.0 -. exp (alpha *. ( 1.0 -. r_a))) in
+    sum *. factor1 *. factor2 +. constant
 
 end
--- src/test.ml
+++ src/test.ml
@@ -1,7 +1,9 @@
 
+open Printf
+
 (* Example usage *)
 
-(* firs, define your score_label module *)
+(* first, define your score_label module *)
 module SL = struct
   type t = string * float * int * bool
   let get_score (_, s, _, _) = s
@@ -10,6 +12,9 @@ end
 
 (* second, instantiate the ROC functor for your score_label module *)
 module ROC = MakeROC.Make (SL)
+
+let almost_equal epsilon x_curr x_ref =
+  (x_ref -. epsilon <= x_curr) && (x_curr <= x_ref +. epsilon)
 
 (* third, call any classification performance metric you need *)
 let main () =
@@ -35,6 +40,21 @@ let main () =
   assert(ROC.power_metric 0.35 scores = tpr_x /. (tpr_x +. fpr_x));
   assert(ROC.auc [("", 1.0, 0, true) ; (("", 0.9, 1, false))] = 1.0);
   assert(ROC.auc [("", 1.0, 0, false); (("", 0.9, 1, true))] = 0.0);
-  Printf.printf "all OK\n"
+  let scores_02 =
+    [ create "" 0.1 0 false;
+      create "" 0.2 0 false;
+      create "" 0.3 0 true;
+      create "" 0.4 0 false;
+      create "" 0.5 0 false;
+      create "" 0.6 0 true;
+      create "" 0.7 0 true;
+      create "" 0.8 0 true;
+      create "" 0.9 0 false;
+      create "" 1.0 0 true ] in
+  (* cross validated with 'croc-curve < test.scored-label > /dev/null' *)
+  assert(ROC.auc scores_02 = 0.76);
+  (* cross validated with 'croc-bedroc < test.scored-label > /dev/null' *)
+  assert(almost_equal 0.0001 (ROC.bedroc_auc scores_02) 0.88297);
+  printf "all OK\n"
 
 let () = main ()
