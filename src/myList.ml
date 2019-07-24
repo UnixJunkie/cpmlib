2f54d82fb7955134e946934197f5cebea007f0cc
Francois Berenger <unixjunkie@sdf.org>
2019-07-24 14:53:35 +0900

    added MyList module

--- src/myList.ml
+++ src/myList.ml
@@ -3,7 +3,7 @@ include BatList
 
 (* count elements of [l] satisfying [p] *)
 let filter_count p l =
-  List.fold_left (fun acc x ->
+  fold_left (fun acc x ->
       if p x then acc + 1
       else acc
     ) 0 l
@@ -12,8 +12,8 @@ let filter_count p l =
    the first one being the longest prefix of [l] where [p] holds *)
 let partition_while p l =
   let rec loop acc = function
-    | [] -> (L.rev acc, [])
+    | [] -> (rev acc, [])
     | x :: xs ->
       if p x then loop (x :: acc) xs
-      else (L.rev acc, x :: xs) in
+      else (rev acc, x :: xs) in
   loop [] l
